2a00fb60c9824d614ec49f67fa7cd394e632e1ac
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-27 10:48:13 -0300

    Criado logica ao clicar no menu abrir activity cadastro e adicionado label na activity cadastro no manisfest

--- app/src/main/java/br/com/fiap/a2tina_android_sqlite/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_sqlite/MainActivity.java
@@ -1,9 +1,11 @@
 package br.com.fiap.a2tina_android_sqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = new MenuInflater(this);
         menuInflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.mnCadastrar){
+            Intent it = new Intent(this, CadastroActivity.class);
+            startActivity(it);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
