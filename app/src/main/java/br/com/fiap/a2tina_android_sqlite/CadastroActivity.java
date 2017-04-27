a2270644f3656f4b2143caacb00851093fdf05a4
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-27 10:50:54 -0300

    Estilizando activity cadastro com edittext, textview e button

--- app/src/main/java/br/com/fiap/a2tina_android_sqlite/CadastroActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_sqlite/CadastroActivity.java
@@ -2,6 +2,7 @@ package br.com.fiap.a2tina_android_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void Salvar(View view) {
     }
 }
