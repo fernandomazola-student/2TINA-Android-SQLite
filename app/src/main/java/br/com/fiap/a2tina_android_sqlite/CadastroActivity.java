32d92ec73e0e8a8a79eb0a963335c6b7efce84ee
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-27 10:52:13 -0300

    Referenciando itens da activitycadastro

--- app/src/main/java/br/com/fiap/a2tina_android_sqlite/CadastroActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_sqlite/CadastroActivity.java
@@ -3,13 +3,20 @@ package br.com.fiap.a2tina_android_sqlite;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    EditText edtNome;
+    EditText edtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
     }
 
     public void Salvar(View view) {
