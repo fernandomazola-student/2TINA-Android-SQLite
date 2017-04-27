250fc58af38c6a4abf41fcc1db965ba63d4ebf51
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-27 11:19:43 -0300

    Override no metodo tostring para nao exibir somente o endereço de memória

--- app/src/main/java/br/com/fiap/a2tina_android_sqlite/Cliente.java
+++ app/src/main/java/br/com/fiap/a2tina_android_sqlite/Cliente.java
@@ -35,4 +35,9 @@ public class Cliente implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return this.nome + " . " + this.email;
+    }
 }
